ce28456ccdc293a1a895640fab582d98595f646b
Layla A <lights@reachinghalcyon.com>
2020-08-22 22:59:48 -0400

    Begin defining the structure of the Engine Layer with skeletal versions of the Engine, Editor, and Login classes. Awaiting documentation.

--- public/engine.js
+++ public/engine.js
@@ -6,3 +6,105 @@
 // for the purpose of a modular design and compatibility going
 // forward.
 // - Layla A
+
+class Editor {
+    constructor(data) {
+        // Viewport used by the renderer
+        this.viewport = {
+            // TODO - Define Me
+        };
+
+        // Currently Loaded chart data
+        this.chart = null;
+
+        // Current State of the editor process
+        this.state = {
+            // TODO - Define Me
+        };
+
+        // Object definitions for Notes and Timings
+        this.schema = data.schema;
+
+        // TODO - implement addon functions
+        this.logic = Editor.Logic(data.logic);
+
+        // TODO - add support for chart serializers that handle note data
+        this.serializer = Editor.Serializer(data.serializer);
+    }
+
+    // TODO - Implement these portions of the engine
+    static Logic() {}
+    static Serializer() {}
+}
+
+class Login {
+    constructor(data) {
+        this.key = data.key || "";
+        this.userid = data.userid || 0;
+        this.username = data.username || "";
+        this.group = data.group || 0;
+        this.storage = data.storage || {
+            total: 0,
+            used: 0
+        };
+        this.since = data.since || Date.now();
+    }
+}
+
+class Engine {
+    #editor = null; // Active Chart Editor object
+    #session = new Login(null); // Active Login-Session object
+    constructor() {
+
+    }
+
+    async spawnEditor(id = "") {
+        // TODO - check for non-null editor and notify the user / save chart
+        return new Promise((resolve, reject) => {
+            if (!id) {
+                this.#editor = null;
+                return resolve({code: 0});
+            }
+
+            // TODO - Send Request to backend to collect current editor data.
+            // For now it will be hard-coded
+            let editorData = {}; // Assume this was fetched from backend and parsed as json
+
+            let index = editorData.indexOf[id];
+            if (index < 0) {
+                return reject({
+                    code: 1,
+                    msg: "Invalid Editor id"
+                });
+            }
+
+            this.#editor = new Editor(editorData[index]);
+            return resolve({code: 0})
+        }).catch(err => {
+            return err;
+        });
+    }
+
+    async loadChart(...args) {
+        // TODO - Load a chart into currently loaded editor via user upload or backend storage
+        // We'll only support user upload for now
+    }
+
+    async saveChart(...args) {
+        // TODO - Cache current chart in browser, store on backend if user has an account & space
+    }
+
+    async exportChart(...args) {}
+    async addObject() {}
+    async rmvObject() {}
+    async addTimepoint() {}
+    async rmvTimepoint() {}
+    async setMeta() {}
+    async cycleSnap() {}
+    async cycleSpeed() {}
+    async timeShift() {}
+    async getSchema() {}
+    async getView() {}
+    async zoom() {}
+}
+
