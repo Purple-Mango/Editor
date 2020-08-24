626e6ddb6a62c09bb4d77b7a9e33cac0362dd635
Layla A <lights@reachinghalcyon.com>
2020-08-23 21:17:06 -0400

    Add eslint and some basic style rules.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 // Placeholder - rewrite in progress
 
 // React Imports
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ui from './ui.js';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import ui from "./ui.js"
+import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
   <React.StrictMode>
     <ui />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+ * If you want your app to work offline and load faster, you can change
+ * unregister() to register() below. Note this comes with some pitfalls.
+ * Learn more about service workers: https://bit.ly/CRA-PWA
+ */
+serviceWorker.unregister()
--- src/renderer.js
+++ src/renderer.js
@@ -1,10 +1,12 @@
-// This module will be responsible for controlling the html5 canvas
-// portion of the editor UI. Functions of this module will be received
-// from the UI layer (either directly for trivial events or after
-// being processed by the engine layer and returned. This module should
-// not be responsible for sending information to the engine layer and will
-// not be used for tracking any of the editor's state- only the way
-// it is being rendered. Some elements of the renderer may need to be
-// referenced by the UI layer to emulate rendered elements inside of the
-// DOM for event-handling.
-// - Layla A
+/*
+ * This module will be responsible for controlling the html5 canvas
+ * portion of the editor UI. Functions of this module will be received
+ * from the UI layer (either directly for trivial events or after
+ * being processed by the engine layer and returned. This module should
+ * not be responsible for sending information to the engine layer and will
+ * not be used for tracking any of the editor's state- only the way
+ * it is being rendered. Some elements of the renderer may need to be
+ * referenced by the UI layer to emulate rendered elements inside of the
+ * DOM for event-handling.
+ * - Layla A
+ */
